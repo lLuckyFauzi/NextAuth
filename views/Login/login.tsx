ed427c35a0a601719685910eb2088cf98812de10
Lucky <Luckyfauzi2004@gmail.com>
2022-08-30 06:05:43 +0700

    addCredentials

--- views/Login/login.tsx
+++ views/Login/login.tsx
@@ -1,7 +1,31 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 
 const login = () => {
+    const router = useRouter()
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passInputRef = useRef<HTMLInputElement>(null);
+
+    const onLoginSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        const emailValue = emailInputRef?.current?.value
+        const passValue = passInputRef?.current?.value
+
+        const results = await signIn('credentials', {
+            redirect: false,
+            email: emailValue,
+            password: passValue
+        })
+
+        if(!results?.error) {
+            router.push('/home')
+        }
+        
+    }
+
   return (
     <div>
         <div>
@@ -10,18 +34,18 @@ const login = () => {
                 <p style={{textAlign: 'center'}}>Welcome Back! How’s Your Days? <br /> Please Enter Your Detail.</p>
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <form>
+                <form onSubmit={onLoginSubmit}>
                     <div>
                         <div>
                             <label style={{ textAlign: 'left'}} htmlFor="email">Email</label>
                         </div>
-                        <input type="text" placeholder='Enter Your Email' />
+                        <input type="text" placeholder='Enter Your Email' ref={emailInputRef} />
                     </div>
                     <div>
                         <div>
                             <label style={{ textAlign: 'left'}} htmlFor="password">Password</label>
                         </div>
-                        <input type="text" placeholder='Password' />
+                        <input type="text" placeholder='Password' ref={passInputRef} />
                     </div>
                     <div style={{textAlign: 'right'}}>
                         <a style={{textDecoration: 'none'}} href="#">Forgot Password?</a>
@@ -34,7 +58,7 @@ const login = () => {
                         <button>Sign With Google</button>
                     </div>
                     <div style={{textAlign: 'right'}}>
-                        <Link style={{textDecoration: 'none'}} href="/signup">Create Account</Link>
+                        <Link style={{textDecoration: 'none'}} href="/">Create Account</Link>
                     </div>
                 </form>
             </div>
