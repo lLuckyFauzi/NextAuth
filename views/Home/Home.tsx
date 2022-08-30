2c19bd0be747e6c70f97414e7a40344d92e10e10
Lucky <Luckyfauzi2004@gmail.com>
2022-08-31 06:08:22 +0700

    addLogout

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { getSession, signOut } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const router = useRouter()
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if(!session) {
+        router.push('/')
+      } else {
+        setIsLogin(true)
+        setLoading(false)
+      }
+    })
+  }, [])
+
+  const logoutHandler = () => {
+    signOut()
+  }
+
+  if(loading) {
+    return (
+      <p style={
+        { textAlign: 'center' }
+    }>Loading...</p>
+    )
+  }
+
   return (
     <div>
         <h1 style={
             { textAlign: 'center' }
         }>Welcome</h1>
+        <div style={
+            { textAlign: 'center' }
+        }>
+          <button onClick={()=> {
+            logoutHandler()
+            router.push('/')
+          }}>Logout</button>
+        </div>
     </div>
   )
 }
